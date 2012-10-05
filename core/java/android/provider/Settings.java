1fe1fcd747d752b09e6518660a4ba02591b41baf
AustinSchulz <schulaus@gmail.com>
2012-10-05 01:26:16 -0500

    Allow airplane and silent power toggle (2/2)
    Based upon the system already in place by DvTonder
    
    Change-Id: Ibf2587da48679c039f335085fe9b78d493eac158

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2712,6 +2712,18 @@ public final class Settings {
           */
 	public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
         /**
+         * Whether power menu airplane toggle is enabled
+         * @hide
+         */
+        public static final String POWER_MENU_AIRPLANE_ENABLED = "power_menu_airplane_enabled";
+
+        /**
+         * Whether power menu silent mode is enabled
+         * @hide
+         */
+        public static final String POWER_MENU_SILENT_ENABLED = "power_menu_silent_enabled";
+
+        /**
          * Whether to enable custom rebindings of the actions performed on
          * certain key press events.
          * @hide
@@ -2869,6 +2881,8 @@ public final class Settings {
             POWER_MENU_SCREENSHOT_ENABLED,
             POWER_MENU_REBOOT_ENABLED,
             POWER_MENU_PROFILES_ENABLED,
+            POWER_MENU_AIRPLANE_ENABLED,
+            POWER_MENU_SILENT_ENABLED,
             LOCKSCREEN_VIBRATE_ENABLED,
         };
 
